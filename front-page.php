f111bd993589dccfb676e59c015acc1c7a8c4609
bnkumark <bnkumar_k@yahoo.co.in>
2016-07-17 00:05:57 +0530

    ui changes

--- front-page.php
+++ front-page.php
@@ -5,8 +5,18 @@
 ?>
 
 </header> <!-- / END HOME SECTION  -->
+<div class="home-header-wrap">
+<div class="header-content-wrap">
+<div class="container">
+<h1 class="intro-text" style="color:#039BE5">WELCOME TO POKEDOC</h1>
+<div id="search" class="widget_search">
 
+				<?php get_search_form(); ?>
 
+			</div> 
+</div>
+</div>
+</div>
 <?php
 
 	/* OUR FOCUS SECTION */
@@ -15,5 +25,5 @@
 	/* RIBBON WITH RIGHT SIDE BUTTON */
 
 
-}
+
 get_footer(); ?>
